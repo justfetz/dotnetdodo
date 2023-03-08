4c3048cd843c0e7cb67b5ab2c2cffdd1f8e79168
J Fetzer <jasonmfetzer@gmail.com>
2023-03-08 12:59:16 -0600

    Models and Pages

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using dotnetdodo.Models;
+
+
+namespace dotnetdodo.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(OrderModel order)
+        {
+            // Do something with the order data
+            // You can access the data using the order object that's passed in
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/OrderModel.cs
@@ -0,0 +1,8 @@
+public class OrderModel
+{
+    public string CustomerName { get; set; }
+    public string PaperType { get; set; }
+    public int Width { get; set; }
+    public int Length { get; set; }
+    public int Quantity { get; set; }
+}
