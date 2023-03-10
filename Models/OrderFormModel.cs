c631a498d38864e377cae529da141d8987816fdd
J Fetzer <jasonmfetzer@gmail.com>
2023-03-09 21:20:01 -0600

    added some junk.

--- Models/OrderFormModel.cs
+++ Models/OrderFormModel.cs
@@ -7,25 +7,30 @@ namespace dotnetdodo.Models
 public class OrderFormModel : PageModel
 {
 
-
     public List<int> Widths { get; set; }
     public List<int> Demands { get; set; }
     public int StockItemWidth { get; set; }
      public List<string> Inputs { get; set; } = new List<string>();
 
-public async Task<IActionResult> OnPostAsync(ModelStateDictionary ModelState)
+public IActionResult OnPostAsync()
 {
     if (!ModelState.IsValid)
     {
         return Page();
     }
 
-    // add the user inputs to the Inputs list in the OrderFormModel
+    // Add the user inputs to the Inputs list in the OrderFormModel
     Inputs.Add($"Width: {Widths}, Demand: {Demands}");
 
-    return Page();
+    // Construct a string that contains the input values
+    var inputValues = string.Join("<br>", Inputs);
+
+    // Return the input values as a string to the AJAX request
+    return new JsonResult(new { inputsHtml = inputValues });
 }
-    
-}
+
+
+
+    }
 
 }
---
+++ Services/CuttingStockSolver.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using Google.OrTools.LinearSolver;
+
+public class CuttingStockSolver
+{
+    public static List<int> Solve(int stockLength, int[] widths, int[] demands)
+    {
+        // Initialize solver
+        Solver solver = new Solver("CuttingStockSolver", Solver.OptimizationProblemType.CLP_LINEAR_PROGRAMMING);
+        
+        // Define variables
+        List<Variable> x = new List<Variable>();
+        foreach (int width in widths)
+        {
+            x.Add(solver.MakeIntVar(0, int.MaxValue, "x" + width));
+        }
+        
+        // Define objective
+        Objective obj = solver.Objective();
+        obj.SetMinimization();
+        obj.SetCoefficient(x[0], 1);
+        
+        // Define constraints
+        foreach (int demand in demands)
+        {
+            Constraint constraint = solver.MakeConstraint(demand, int.MaxValue);
+            for (int i = 0; i < x.Count; i++)
+            {
+                constraint.SetCoefficient(x[i], widths[i]);
+            }
+        }
+        
+        // Solve problem
+        solver.Solve();
+        
+        // Get solution
+        List<int> solution = new List<int>();
+        foreach (Variable var in x)
+        {
+            int count = Convert.ToInt32(Math.Round(var.SolutionValue()));
+            for (int i = 0; i < count; i++)
+            {
+                solution.Add(var.Name().Contains("x") ? Convert.ToInt32(var.Name().Substring(1)) : 0);
+            }
+        }
+        return solution;
+    }
+}
