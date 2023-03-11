b8c56c35f0fea17ab3309b844c9ede6506a3047f
J Fetzer <jasonmfetzer@gmail.com>
2023-03-11 08:31:32 -0600

    broke to test the order form

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,18 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-using System.Collections.Generic;
-using System.Linq;
 
 namespace dotnetdodo.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly CuttingStockSolver _cuttingStockSolver;
-
-        public IndexModel(CuttingStockSolver cuttingStockSolver)
-        {
-            _cuttingStockSolver = cuttingStockSolver;
-        }
+        
 
         public void OnGet()
         {
